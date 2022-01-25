f5fde27c5d98e10ba92d2b1ebecc8f87aaac09c2
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-01-25 12:11:09 -0500

    added userform component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import UserForm from './UserForm'
 function App() {
   return (
     <div className="App">
+      <UserForm />
     </div>
   );
 }
---
+++ src/UserForm.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const UserForm = () => {
+  return (
+    <form>
+      <input type='text' placeholder='username'/>
+      <button>Add me to List</button>
+    </form>
+  );
+}
+
+export default UserForm;
