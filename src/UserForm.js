2af1bfbd893f734d72da4d41a30b3c99c56f9af7
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-01-26 11:18:38 -0500

    change both states to empty string, add submit methods, add email input

--- src/UserForm.js
+++ src/UserForm.js
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import "./UserForm.css";
 
 const UserForm = () => {
-  const [username, setUsername] = useState("ChickenLady");
-  const handleChange = (e) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const handleUsernameChange = (e) => {
     setUsername(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,12 +17,24 @@ const UserForm = () => {
   };
   return (
     <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username</label>
       <input
+        id="username"
         type="text"
         placeholder="username"
         value={username}
-        onChange={handleChange}
+        onChange={handleUsernameChange}
       />
+
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        placeholder="email"
+        id="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+
       <button>Submit</button>
     </form>
   );
