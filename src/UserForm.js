45f30738bd15c11ff18e207ec30d1c046c337769
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-01-26 10:16:48 -0500

    added state to change ,on the value of the input

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 
 const UserForm = () => {
-  const [username, setUsername] = useState('ChickenLady')
+  const [username, setUsername] = useState("ChickenLady");
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
   return (
     <form>
-      <input type='text' placeholder='username'value={username}
-      onChange={() => console.log('change')}/>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={handleChange}
+      />
       <button>Add me to List</button>
     </form>
   );
-}
+};
 
 export default UserForm;
