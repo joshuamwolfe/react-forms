042c082eea5d2a68af041c1dfdc390c60f7821d5
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-01-26 11:34:18 -0500

    combined state into one, removed form handlers, combined handle change,

--- src/UserForm.js
+++ src/UserForm.js
@@ -2,37 +2,44 @@ import React, { useState } from "react";
 import "./UserForm.css";
 
 const UserForm = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target.value;
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
   };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Created User, ${username}`);
-    setUsername("");
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   alert(`Created User, ${username}`);
+  //   setUsername("");
+  // };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <label htmlFor="username">Username</label>
       <input
         id="username"
         type="text"
+        name="username"
         placeholder="username"
-        value={username}
-        onChange={handleUsernameChange}
+        value={formData.username}
+        onChange={handleChange}
       />
 
       <label htmlFor="email">Email</label>
       <input
         type="email"
         placeholder="email"
+        name="email"
         id="email"
-        value={email}
-        onChange={handleEmailChange}
+        value={formData.email}
+        onChange={handleChange}
       />
 
       <button>Submit</button>
