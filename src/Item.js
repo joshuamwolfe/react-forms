667dd80bde2c189af53acaff666be295834bebd1
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-02-01 11:41:14 -0500

    final form changes

--- src/Item.js
+++ src/Item.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Item = ({ id, name, qty }) => {
+
   return (
     <ul>
       <li>Product Name: {name}</li>
       <li>Quantity: {qty}</li>
     </ul>
-  );
-};
+  )
+
+}
 
 export default Item;
--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
-import Item from "./Item";
+import { v4 as uuid } from 'uuid';
+import Item from "./Item"
+import NewItemForm from "./NewItemForm";
 
 const ShoppingList = () => {
-  const INTIAL_STATE = [
-    { id: 1, name: "Peanut Butter", qty: 2 },
-    { id: 1, name: "Whole Milk", qty: 1 },
-  ];
-  const [items, setItems] = useState(INTIAL_STATE);
-
+  const INITIAL_STATE = [
+    { id: uuid(), name: 'Peanut Butter', qty: 2 },
+    { id: uuid(), name: 'Whole Milk', qty: 1 }
+  ]
+  const [items, setItems] = useState(INITIAL_STATE);
+  const addItem = (newItem) => {
+    setItems(items => [...items, { ...newItem, id: uuid() }])
+  }
   return (
     <div>
       <h3>Shopping List</h3>
+      <NewItemForm addItem={addItem} />
       <div>
-        {items.map(({ id, name, qty }) => (
-          <Item id={id} name={name} qty={qty} />
-        ))}
+        {items.map(({ id, name, qty }) => <Item id={id} name={name} qty={qty} key={id} />)}
       </div>
+
     </div>
-  );
-};
+  )
+
+}
 
 export default ShoppingList;
-
-// ul>li{My Item $}*3
